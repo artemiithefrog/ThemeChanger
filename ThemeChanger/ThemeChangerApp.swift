3235fa0310a54d4782418fe8f3d517f266acfbf2
Artemii <artemiithefrog95@icloud.com>
2023-12-23 16:27:05 +0100

    Initial Commit

---
+++ ThemeChanger/ThemeChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeChangerApp.swift
+//  ThemeChanger
+//
+//  Created by artemiithefrog . on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ThemeChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
